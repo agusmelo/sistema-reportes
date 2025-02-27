5ccddaaa56b4700e78d5a8b246eedca537b04980
Agustin Melo <agustin.melo.olariaga@fing.edu.uy>
2025-02-27 19:48:41 -0300

    functines del controller cliente

--- src/controllers/clientController.js
+++ src/controllers/clientController.js
@@ -1,57 +1,95 @@
-const path = require('path');
-const db = require(path.join(__dirname,'../db/connect_db'))
+const ClientModel = require('../model/clientes');
 
-// Manejador para crear un nuevo usuario
-exports.createUser = (req, res) => {
-    // Lógica para crear un usuario
-    const {name, telefono} = req.body;
-
+// TODO: agregar error handling y validacion de tipos
+exports.createClient = async (req, res) => {
+    const {nombre} = req.body;
     try {
-        agregarCliente(name, telefono)
+        await ClientModel.agregarCliente(nombre);
+        res.status(201).json({
+            message: "Usuario creado con éxito",
+            nombre: nombre
+        });
+
     } catch (error) {
-        
+        res.status(500).json({
+            message: "Error al crear el usuario",
+            error: error.message
+        });      
     }
-    res.status(201).send("Usuario creado");
 };
 
-// Manejador para obtener todos los usuarios
-exports.getAllUsers = (req, res) => {
-    // Lógica para obtener todos los usuarios
-    // console.log(req)
-    
-    res.status(200).send("Lista de usuarios");
-};
-
-// Manejador para obtener un usuario por ID
-exports.getUserById = (req, res) => {
-    const clientId = req.params.id;
+exports.getAllClients = async (req, res) => {
     try {
-        const dataCliente = obtenerCliente(clientId);
-        res
-        .status(200)
-        .json({
-            message: "Cliente " + clientId,
-            data: dataCliente
+        const clientes = await ClientModel.obtenerClientes();
+        console.log('clientes: ', clientes)
+        res.status(200).json({
+            message: "Lista de clientes obtenida con éxito",
+            clientes: clientes
         });
     } catch (error) {
-        res
-            .status(500)
-            .json({
-                message:"Error obteniendo cliente",
-            })
+        res.status(500).json({
+            message: "Error al crear el usuario",
+            error: error.message
+        });   
     }
 };
 
-// Manejador para actualizar un usuario
-exports.updateUser = (req, res) => {
-    const userId = req.params.id;
-    // Lógica para actualizar un usuario
-    res.status(200).send(`Usuario con ID: ${userId} actualizado`);
+
+exports.getClientById = async (req, res) => {
+    const clientId = req.params.id;
+    try {
+        const dataCliente = await ClientModel.obtenerCliente(clientId);
+        if(dataCliente.length === 1){
+            res.status(200).json({
+                message: `Cliente ${clientId}`,
+                data: dataCliente
+            });
+        }
+        else if(dataCliente.length === 0){
+            res.status(404).json({
+                message: `No existe cliente con id = ${clientId}`,
+            });
+        }
+        else{
+            
+            res.status(500).json({
+                message: `DEV ERROR: Mas de un cliente con id: ${clientId}, algo esta mal.`,
+            });
+        }
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({
+            message:"Error obteniendo cliente",
+        })
+    }
 };
 
-// Manejador para eliminar un usuario
-exports.deleteUser = (req, res) => {
-    const userId = req.params.id;
-    // Lógica para eliminar un usuario
-    res.status(204).send();
+// TODO: revisar los parametros 
+exports.updateClient = async (req, res) => {
+    const {id:userId, data} = req.params;
+    try {
+        const data = await ClientModel.actualizarCliente(clientId, data);
+        res.status(200).json({
+            message: `Cliente ${clientId} actualizado correctamente`
+        });
+    } catch (error) {
+        res.status(500).json({
+            message:"Error actualizando cliente",
+        })
+    }
+};
+
+exports.deleteClient = async (req, res) => {
+    const clientId = req.params.id;
+    try {
+        await ClientModel.eliminarCliente(clientId);
+        res.status(200).json({
+            message: `Cliente ${clientId} eliminado correctamente`
+        });
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({
+            message: `Cliente ${clientId} no se pudo eliminar`
+        });
+    }
 };
