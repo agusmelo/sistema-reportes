0c63e871e3b181925879ab2ab147cab3ecb461fd
Agustin Melo <agustin.melo.olariaga@fing.edu.uy>
2025-02-27 19:45:22 -0300

    +sqlite async/await support

--- src/db/connect_db.js
+++ src/db/connect_db.js
@@ -1,14 +1,39 @@
 const path = require('path');
-const Sqlite3 = require('sqlite3');
-const sql3 = Sqlite3.verbose();
+const sqlite3 = require('sqlite3');
+const sql3 = sqlite3.verbose();
+const { open } = require('sqlite')
 
-const DB = new sql3.Database(path.join(__dirname,'./database.db'),Sqlite3.OPEN_READWRITE,connected);
+let db = null
 
-function connected(err){
-    if(err){
-        console.log(err.message)
-        return;
+
+loadTables()
+
+async function connectDB() {
+    if (!db) {
+        try {
+            db = await open({
+                filename: path.join(__dirname, "database.db"),
+                driver: sqlite3.Database,
+            })
+            console.log("Database connected.")
+        } catch (error) {
+            console.error("Error connecting to the database:", err.message)
+            throw new Error("Database connection failed")
+        }
     }
+    return db
+}
+
+async function closeDB() {
+    if (db) {
+      await db.close()
+      console.log("Database closed.")
+      db = null
+    }
+  }
+
+async function loadTables(){
+    const db = await connectDB();
     const tables = {
         facturas:`CREATE TABLE IF NOT EXISTS facturas (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -41,26 +66,14 @@ function connected(err){
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         );`,
     }
-
-    DB.run(tables.facturas, [], (err) => {
-        if(err)
-            console.log("Error creating 'facturas' table");
-            return;
-    })
-
-    DB.run(tables.clientes, [], (err) => {
-        if(err)
-            console.log("Error creating 'clientes' table");
-            return;
-    })
-
-    DB.run(tables.items, [], (err) => {
-        if(err)
-            console.log("Error creating 'items' table");
-            return;
-    })
-    console.log("Table created")
+    for (const table in tables) {
+        db.run(tables[table], [], (err) => {
+            if(err)
+                console.log(`Error creating ${table} table`);
+                return;
+        })
+    }
+    console.log("Tables created")
 }
 
-
-module.exports = {DB}
+module.exports = { connectDB, closeDB }
