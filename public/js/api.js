6842457ea59d6f83d876bbe3542880f1a2774aec
Agustin Melo <agustin.melo.olariaga@fing.edu.uy>
2025-03-17 07:22:14 -0300

    (front) Added interceptor to handle response errors + log

--- public/js/api.js
+++ public/js/api.js
@@ -5,4 +5,50 @@ const api = axios.create({
   },
 });
 
+api.interceptors.response.use(
+  (response) => response.data,
+  (error) => {
+    if (error.response) {
+      const { status, data } = error.response;
+
+      switch (status) {
+        case 400:
+          console.warn("Bad Request:", data.message);
+          break;
+        case 401:
+          console.warn("Unauthorized!");
+          // localStorage.removeItem("token");
+          // window.location.href = "/login";
+          break;
+        case 403:
+          console.warn("Forbidden: You do not have permission.");
+          break;
+        case 404:
+          console.warn("Resource Not Found:", data.message);
+          break;
+        case 500:
+          console.error("Server Error:", data.message);
+          break;
+        default:
+          console.error("Unexpected Error:", data.message);
+      }
+    } else if (error.request) {
+      console.error("No response received. Check your network.");
+    } else {
+      console.error("Request Error:", error.message);
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+//TODO: Add retry when an error occurs?
+// async (error) => {
+//   if (error.response?.status === 500) {
+//     console.warn("Server error, retrying request...");
+//     return api.request(error.config); // Retry the original request
+//   }
+//   return Promise.reject(error);
+// };
+
 export default api;
