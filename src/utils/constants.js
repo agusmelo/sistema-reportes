06c46029536b313183ec8ff058e1270fb700e035
Agustin Melo <agustin.melo.olariaga@fing.edu.uy>
2025-03-20 20:12:23 -0300

    more decoupling

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,22 @@
 // TODO: crear constantes para standarizar los mensajes con el servidor
+const MESES = [
+  "Enero",
+  "Febrero",
+  "Marzo",
+  "Abril",
+  "Mmayo",
+  "Junio",
+  "Julio",
+  "Agosto",
+  "Septiembre",
+  "Octubre",
+  "Noviembre",
+  "Diciembre",
+];
+
+const HORIZONTAL_SPACE = "    ";
+
+module.exports = {
+  MESES,
+  HORIZONTAL_SPACE,
+};
