45d571835a1dbb045ec8469351a40b381af71a76
Agustin Melo <agustin.melo.olariaga@fing.edu.uy>
2025-03-29 00:43:01 -0300

    custom logs

--- src/utils/customLog.js
+++ src/utils/customLog.js
@@ -1,26 +1,51 @@
-const chalk = require("chalk");
+import chalk from "chalk";
 
-const Logger = (message, type) => {
-  switch (type) {
-    case "dev":
-      console.log(chalk.cyan(`[D] ${message}`));
-      break;
-
-    case "warn":
-      console.log(chalk.yellow(`[!] ${message}`));
-      break;
-
-    case "help":
-      console.log(chalk.blue(`[?] ${message}`));
-      break;
-
-    case "success":
-      console.log(chalk.blue(`[☑] ${message}`));
-      break;
-    default:
-      console.log(message);
-      break;
+class Logger {
+  static dev(message) {
+    console.log(chalk.cyan(`[D] ${message}`));
   }
-};
+  static warn(message) {
+    console.log(chalk.yellow(`[!] ${message}`));
+  }
+  static error(message) {
+    console.log(chalk.red(`[X] ${message}`));
+  }
+  static help(message) {
+    console.log(chalk.blue(`[?] ${message}`));
+  }
+  static success(message) {
+    console.log(chalk.blue(`[☑] ${message}`));
+  }
+  static info(message) {
+    console.log(chalk.white(`[i] ${message}`));
+  }
+}
 
-module.exports = { Logger };
+// const Logger = (message, type) => {
+//   switch (type) {
+//     case "dev":
+//       console.log(chalk.cyan(`[D] ${message}`));
+//       break;
+
+//     case "warn":
+//       console.log(chalk.yellow(`[!] ${message}`));
+//       break;
+
+//     case "error":
+//       console.log(chalk.red(`[X] ${message}`));
+//       break;
+
+//     case "help":
+//       console.log(chalk.blue(`[?] ${message}`));
+//       break;
+
+//     case "success":
+//       console.log(chalk.blue(`[☑] ${message}`));
+//       break;
+//     default:
+//       console.log(message);
+//       break;
+//   }
+// };
+
+export default Logger;
